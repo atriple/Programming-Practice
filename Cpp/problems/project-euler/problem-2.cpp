7097ea98767e3ced69b308657808dea4cf95488c
atriple <andiaqil.dpe@gmail.com>
2019-03-16 00:12:02 +0700

    Problems 2 Project Euler

--- Cpp/problems/project-euler/problem-2.cpp
+++ Cpp/problems/project-euler/problem-2.cpp
@@ -1,9 +1,29 @@
-//Problems : 
+/*
+Problems :
+By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
+find the sum of the even-valued terms.
+*/
 #include<iostream>
-#include<cstdio>
 
 int main() {
+	int sum = 0;
 
+	//Fibonacci term
+	int term1 = 1, term2 = 1, term3 = term1 + term2;
+	
+
+	while (!(term3 > 4 * 1000000)) {
+		if (term3 % 2 == 0) {
+			std::cout << term3 << std::endl;
+			sum += term3;
+		}
+		term1 = term2;
+		term2 = term3;
+		term3 = term1 + term2;
+	}
+
+	std::cout << sum << std::endl;
+	system("pause");
 	
 	return 0;
 }
