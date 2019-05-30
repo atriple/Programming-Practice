d1418eb047fe6c257adc4266f48d2348e3500d1e
atriple <andiaqil.dpe@gmail.com>
2019-05-30 15:13:03 +0700

    Some fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Competitive-Programming
-competitive programming templates and problem solutions.
+Mainly for competitive programming templates and problem solutions. 
+Also can be used to store some small problems, algorithms, data structure, coding interview solution code in general
---
+++ competitions/gcj-2019/round1C/rps.py
@@ -0,0 +1,22 @@
+# Goals : To able to compete with every robots registered
+import string
+
+def counter_solution(C):
+    pass
+
+t = int(input()) #read a line with a single integer
+for i in range(1, t + 1):
+    A = int(input()) # How many other robots
+    C = [] #the program of each robots
+    output = ""
+    print(type(C))
+    for x in range(0,A):
+        inp = input() # program input
+        C.append(inp)
+        
+    
+    sol = [] #Solution of counter on each scenario.
+    
+        
+
+    print("Case #{}: {}".format(i, output))
