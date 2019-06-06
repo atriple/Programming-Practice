b5a23640d1563c9e37ab5ff2b02ec557ad6f73a6
atriple <andiaqil.dpe@gmail.com>
2019-06-06 22:12:28 +0700

    Add code_test dir

---
+++ code_test/test_con.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	bool x = (3 == 4);
+	cout << (2 == 2) << endl;
+	cout << x << endl;
+	cout << ("Saruman" != "saruman") << endl;
+	cout << ("Manjaro" == "Manjaro");
+	system("pause");
+
+	return 0;
+}
