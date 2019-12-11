482fa9a730b81f5fc6f704fb8db6e9914e65a37e
atriple <andiaqil.dpe@gmail.com>
2019-12-11 23:35:38 +0700

    exercism(js) : answer guess for problem 4

--- problems/exercism/javascript/README.md
+++ problems/exercism/javascript/README.md
@@ -8,4 +8,6 @@ node modules di move aja karena hampir semuanya sama...
 2. [Two Fer]()
 3. [Resistor Color]()
     1. [Leap]()
+    2. [Reverse String]()
+    3. [Triangle]()
 4. [Resistor Color Duo]()
--- problems/exercism/javascript/resistor-color-duo/resistor-color-duo.js
+++ problems/exercism/javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const value = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const value = (colorPair) => COLORS.indexOf(colorPair[0]) * 10 + COLORS.indexOf(colorPair[1]);
+
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
