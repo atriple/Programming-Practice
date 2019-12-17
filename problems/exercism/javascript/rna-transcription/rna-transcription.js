bd8c5699e6d86e573bbccf964c07f538cd619e01
atriple <andiaqil.dpe@gmail.com>
2019-12-18 05:11:24 +0700

    exercism(js) : functional solution for [rna transcription]

--- problems/exercism/javascript/rna-transcription/rna-transcription.js
+++ problems/exercism/javascript/rna-transcription/rna-transcription.js
@@ -10,10 +10,17 @@ const NUCLEOTIDE_COMPLEMENT = {
   'A' : 'U'
 };
 
-export const toRna = (nucleotides) => {
-  let complements = '';
-  for(let n of nucleotides){
-    complements = complements + NUCLEOTIDE_COMPLEMENT[n];
-  }
-  return complements;
-};
+//Functional Solution
+const complement = (n) => NUCLEOTIDE_COMPLEMENT[n];
+
+export const toRna = (nucleotides) => nucleotides.split('').map(complement).join('');
+// We can also use reduce() instead of join(), but keep it simple, stupid.
+
+//Initial Solution
+// export const toRna = (nucleotides) => {
+//   let complements = '';
+//   for(let n of nucleotides){
+//     complements = complements + NUCLEOTIDE_COMPLEMENT[n];
+//   }
+//   return complements;
+// };
