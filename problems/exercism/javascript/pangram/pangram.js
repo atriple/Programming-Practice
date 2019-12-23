0624d0d2bf63def30a4659cb4dc514e7557b55a1
atriple <andiaqil.dpe@gmail.com>
2019-12-23 09:47:52 +0700

    exercism(js) : sol to [pangram]

--- problems/exercism/javascript/pangram/pangram.js
+++ problems/exercism/javascript/pangram/pangram.js
@@ -3,6 +3,24 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPangram = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isPangram = (str) => {
+  let pangram = true;
+
+  //Generate object {a : false, b : false, ..., z: false}, I'm not sure if this is a good practice...
+  let LETTER_CHECKLIST = 'abcdefghijklmnopqrstuvwxyz'.split('').reduce(
+    (list = {}, alphabet) => {
+      list[alphabet] = false;
+      return list;
+    }, {});
+  
+  //Check
+  str.toLowerCase().split('').map((ch) => {
+      if (LETTER_CHECKLIST[ch] == false) LETTER_CHECKLIST[ch] = true;
+    });
+
+  for(let key in LETTER_CHECKLIST){
+    if (LETTER_CHECKLIST[key] == false) pangram = false;
+  }
+
+  return pangram;
 };
