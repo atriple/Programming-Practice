21af9b176fb484b2b12e7ccf0462c6aee25901cb
atriple <andiaqil.dpe@gmail.com>
2019-12-25 10:18:10 +0700

    exercism(js): better solution to [pangram]

--- problems/exercism/javascript/pangram/pangram.js
+++ problems/exercism/javascript/pangram/pangram.js
@@ -2,25 +2,6 @@
 // This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const ALPHABETS = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
-export const isPangram = (str) => {
-  let pangram = true;
-
-  //Generate object {a : false, b : false, ..., z: false}, I'm not sure if this is a good practice...
-  let LETTER_CHECKLIST = 'abcdefghijklmnopqrstuvwxyz'.split('').reduce(
-    (list = {}, alphabet) => {
-      list[alphabet] = false;
-      return list;
-    }, {});
-  
-  //Check
-  str.toLowerCase().split('').map((ch) => {
-      if (LETTER_CHECKLIST[ch] == false) LETTER_CHECKLIST[ch] = true;
-    });
-
-  for(let key in LETTER_CHECKLIST){
-    if (LETTER_CHECKLIST[key] == false) pangram = false;
-  }
-
-  return pangram;
-};
+export const isPangram = (str) => (ALPHABETS.every((letter) => str.toLowerCase().includes(letter)));
