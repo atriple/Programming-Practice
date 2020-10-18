6690f98ad657f98dd59972deeebd9b25e866edc4
Andi Aqil A <40452321+atriple@users.noreply.github.com>
2020-10-18 10:35:11 +0700

    py: Fix and optimize `island.py`

--- python/algorithm/dfs/island.py
+++ python/algorithm/dfs/island.py
@@ -7,49 +7,43 @@ ISLAND_MAP = ((1, 1, 0, 0, 0),
 
 
 def search_island(island_map):
-    """Search for island
+    """Search for island from given map
 
     Args:
-        ...
+        island_map (tuple<tuple>) : 2 dimensional array consist of 1 or 0's
 
     Returns:
         int: number of island(s)
     """
 
     # TODO: Make it more efficient with caching
-    # TODO: Make it more functional/declarative
     # TODO: Handle exceptions
 
-    map_size = {"row": len(island_map), "col": len(island_map[0])}
-    visited_node = [[0] * map_size["col"]] * map_size["row"]
+    MAP_SIZE = {"row": len(island_map), "col": len(island_map[0])}
+    visited_node = [[0] * MAP_SIZE["col"] for _ in range(MAP_SIZE["row"])] 
     island_count = 0
 
-    def depth_first_search(island_map, visited_node, row, col):
-        map_size = {"row": len(island_map), "col": len(island_map[0])}
-        print(visited_node)
-        print(row, col)
+    def depth_first_search(island_map, visited_node, row, col, map_size=MAP_SIZE):
         visited_node[row][col] = 1
         for i in [-1, 0, 1]:
             for j in [-1, 0, 1]:
                 adjacent_row = row + i
                 adjacent_col = col + j
-                isRowInBound = adjacent_row >= 0 and adjacent_row < map_size["row"]
-                isColInBound = adjacent_col >= 0 and adjacent_col < map_size["col"]
+                isRowNotInBound = adjacent_row < 0 or adjacent_row >= MAP_SIZE["row"]
+                isColNotInBound = adjacent_col < 0 or adjacent_col >= MAP_SIZE["col"]
 
-                if not(isRowInBound) or not(isColInBound):
+                if isRowNotInBound or isColNotInBound:
                     continue
                  
                 if island_map[adjacent_row][adjacent_col] and not(visited_node[adjacent_row][adjacent_col]):
-                    print(adjacent_row, adjacent_col)
                     depth_first_search(island_map, visited_node, adjacent_row, adjacent_col)
 
         return visited_node
 
-    for row in range(map_size["row"]):
-        for col in range(map_size["col"]):
+    for row in range(MAP_SIZE["row"]):
+        for col in range(MAP_SIZE["col"]):
             if island_map[row][col] and not(visited_node[row][col]):
                 visited_node = depth_first_search(island_map, visited_node, row, col)
-                print("d", visited_node)
                 island_count = island_count + 1
     
     return island_count
